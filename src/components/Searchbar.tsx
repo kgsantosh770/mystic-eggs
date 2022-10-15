1fc74e84856aae345216028253b89cf81e9f2fce
Santosh <s.kunga.ganeshbabu@accenture.com>
2022-10-15 11:42:42 +0530

    search bar component created and image declaration

---
+++ src/components/Searchbar.tsx
@@ -0,0 +1,22 @@
+import SearchIcon from "../assets/images/icons/search.svg";
+
+interface Props {
+    placeholder: string,
+    className?: string,
+}
+
+export default function Searchbar(props: Props) {
+    return (
+        <div className={`relative w-1/2 max-w-screen-lg flex items-center mb-[5px] ${props.className}`}>
+            <img
+                className="w-[22px] h-[22px] absolute left-[20px]"
+                src={SearchIcon}
+                alt="search"
+            />
+            <input
+                className='searchbar rounded-full focus:outline-none w-full py-[10px] pl-[50px] pr-[20px]'
+                type="search"
+                placeholder={props.placeholder} />
+        </div>
+    )
+}
