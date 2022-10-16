4aafe792541e50b11dcbf26a86661aa83a078336
Santosh <s.kunga.ganeshbabu@accenture.com>
2022-10-16 19:45:04 +0530

    sidebar design change

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,30 +1,35 @@
 import { useState } from "react"
+import { PRODUCT_NAME } from "../constants/Constant.ts";
 
 import Logo from "../assets/images/logo.png";
 import HomeIcon from "../assets/images/icons/home.svg";
-import CollectionsIcon from "../assets/images/icons/collections.svg";
-import ProfileIcon from "../assets/images/icons/profile.svg";
-import BagIcon from "../assets/images/icons/bag.svg";
+import ExploreIcon from "../assets/images/icons/explore.svg";
+import CartIcon from "../assets/images/icons/cart.svg";
+import TrendingIcon from "../assets/images/icons/trending.svg";
+import WalletIcon from "../assets/images/icons/wallet.svg";
 
 export default function Sidebar() {
     const [isExpanded, setIsExpanded] = useState(false)
     return (
-        <nav className="fixed top-0 h-full">
-            <img className="logo mt-6 mb-12 rounded-full mx-auto" src={Logo} alt="Logo" />
+        <nav className="fixed top-0 h-full w-14 flex flex-col place-self-center">
+            <button className="mx-auto my-7">
+                <img className="logo" src={Logo} alt={PRODUCT_NAME} title={PRODUCT_NAME} />
+            </button>
             <ul>
                 <li>
-                    <button><img src={CollectionsIcon} alt="collections" title="Collections"/> 
-                    {isExpanded && <div>Collections</div>}</button>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <button><img src={ProfileIcon} alt="profile" title="Profile"/> 
-                    {isExpanded && <div>Profile</div>}</button>
+                    <button><img src={HomeIcon} alt="home" title="Home" /></button>
                 </li>
                 <li>
-                    <button><img src={BagIcon} alt="my-nfts" title="My Nfts"/> 
-                    {isExpanded && <div>My NFT's</div>}</button>
+                    <button><img src={ExploreIcon} alt="explore" title="Explore" /></button>
+                </li>
+                <li>
+                    <button><img src={CartIcon} alt="cart" title="Cart" /></button>
+                </li>
+                <li>
+                    <button><img src={TrendingIcon} alt="trending" title="Trending" /></button>
+                </li>
+                <li>
+                    <button><img src={WalletIcon} alt="wallet" title="Wallet" /></button>
                 </li>
             </ul>
         </nav>
