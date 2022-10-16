8897b110d115aa4be2463c8e57c59e2c5737bbf3
Santosh <s.kunga.ganeshbabu@accenture.com>
2022-10-16 23:03:56 +0530

    fonts moved to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,33 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: [
+        'Nunito-Regular',
+        '"Inter"',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      'base-light' : ['Nunito-Light'],
+      'base-light-italic' : ['Nunito-Light-Italic'],
+      'base-regular' : ['Nunito-Regular'],
+      'base-regular-italic' : ['Nunito-Regular-Italic'],
+      'base-semibold' : ['Nunito-SemiBold'],
+      'base-regular-italic' : ['Nunito-SemiBold-Italic'],
+      'base-bold' : ['Nunito-Bold'],
+      'base-bold-italic' : ['Nunito-Bold-Italic'],
+    }
   },
   plugins: [],
 }
